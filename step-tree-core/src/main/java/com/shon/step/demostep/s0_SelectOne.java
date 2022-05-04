2b824bb3477271ec78cd001e9127af4c8eff9f48
shon <1097213688@qq.com>
2022-05-04 23:50:37 +0800

    step tree

--- step-tree-core/src/main/java/com/shon/step/demostep/s0_SelectOne.java
+++ step-tree-core/src/main/java/com/shon/step/demostep/s0_SelectOne.java
@@ -3,8 +3,8 @@ package com.shon.step.demostep;
 import com.shon.step.core.anotation.Step;
 import com.shon.step.core.template.AbstractStep;
 
-@Step(desc = "开始")
-public class s0_Start extends AbstractStep<DemoStepContext> {
+@Step(desc = "挑选一只大象")
+public class s0_SelectOne extends AbstractStep<DemoStepContext> {
     @Override
     protected void execute(DemoStepContext context) {
 
